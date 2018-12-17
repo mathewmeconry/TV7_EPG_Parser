66283cdc5aa95cafb1fe7faca46d58dc193ec3e8
Nacho Barrientos <nacho.barrientos@gmail.com>
2018-12-17 11:44:04 +0100

    Add tele.ch - Init7 mapping for TVE Internacional
    Tele.ch's id is 'tveinternational'. Init7's id after the prep step is
    'tveinternacional'.
    
     $ curl https://api.init7.net/tvchannels.m3u -s | grep TVE
     #EXTINF:-1,TVE Internacional

--- main.py
+++ main.py
@@ -116,6 +116,8 @@ def match_tele(channel_list, tele_epg):
             channel_id = "rts1"
         elif channel_id == "rtsdeux":
             channel_id = "rts2"
+        elif channel_id == "tveinternational":
+            channel_id = "tveinternacional"
 
         if find_channel_by_id(channel_id, channel_list):
             programm_matched = {
